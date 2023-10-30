c9f5c88a378676233b93f55f0481536f62d7a99f
Lu Peng <bolu.peng@oracle.com>
2023-10-30 17:24:25 -0400

    Updated code.

--- oci_mlflow/auth_plugin.py
+++ oci_mlflow/auth_plugin.py
@@ -4,30 +4,11 @@
 # Copyright (c) 2023 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
-from ads.common.auth import AuthType
-from oci import Signer
-from oci.auth.signers import (
-    get_resource_principals_signer,
-    InstancePrincipalsSecurityTokenSigner,
-    SecurityTokenSigner
-)
-from oci.config import (
-    from_file,
-    DEFAULT_LOCATION, # "~/.oci/config"
-    DEFAULT_PROFILE # "DEFAULT"
-)
-import os
-import oci
+from ads.common.auth import default_signer
 from mlflow.tracking.request_auth.abstract_request_auth_provider import RequestAuthProvider
 
-
-from oci_mlflow import logger, utils
-
 OCI_REQUEST_AUTH = "OCI_REQUEST_AUTH"
-OCIFS_IAM_TYPE = "OCIFS_IAM_TYPE"
-OCI_IAM_TYPE = "OCI_IAM_TYPE"
-OCI_CONFIG_PROFILE = "OCI_CONFIG_PROFILE"
-OCI_CONFIG_LOCATION = "OCI_CONFIG_LOCATION"
+
 
 class OCIMLFlowAuthRequestProvider(RequestAuthProvider):
     
@@ -45,41 +26,4 @@ class OCIMLFlowAuthRequestProvider(RequestAuthProvider):
 
         :return: OCI MLFlow signer
         """
-        auth_type = os.environ.get(OCI_IAM_TYPE) or os.environ.get(OCIFS_IAM_TYPE)
-        file_location = os.environ.get(
-            OCI_CONFIG_LOCATION, DEFAULT_LOCATION
-        )
-        profile_name = os.environ.get(
-            OCI_CONFIG_PROFILE, DEFAULT_PROFILE
-        )
-        if auth_type == AuthType.RESOURCE_PRINCIPAL:
-            return get_resource_principals_signer()
-        elif auth_type == AuthType.INSTANCE_PRINCIPAL:
-            return InstancePrincipalsSecurityTokenSigner()
-        elif auth_type == AuthType.SECURITY_TOKEN:
-            config = from_file(
-                file_location=file_location,
-                profile_name=profile_name
-            )
-            token=utils._read_security_token_file(
-                config.get("security_token_file")
-            )
-            private_key=oci.signer.load_private_key_from_file(
-                filename=config.get("key_file"), 
-                pass_phrase=config.get("pass_phrase")
-            )
-            return SecurityTokenSigner(token=token, private_key=private_key)
-        else:
-            if auth_type != AuthType.API_KEY:
-                logger.info("Invalid or empty auth type. Using api_key as default authentication.")
-            config = from_file(
-                file_location=file_location,
-                profile_name=profile_name
-            )
-            return Signer(
-                tenancy=config.get("tenancy"),
-                user=config.get("user"),
-                fingerprint=config.get("fingerprint"),
-                private_key_file_location=config.get("key_file"),
-                pass_phrase=config.get("pass_phrase")
-            )
+        return default_signer()["signer"]
--- tests/plugins/unitary/test_auth.py
+++ tests/plugins/unitary/test_auth.py
@@ -3,8 +3,7 @@
 
 # Copyright (c) 2023 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
-import os
-from unittest.mock import MagicMock, patch
+from unittest.mock import patch
 from oci_mlflow.auth_plugin import OCIMLFlowAuthRequestProvider
 
 
@@ -13,112 +12,13 @@ class TestOCIMLFlowAuth:
         provider = OCIMLFlowAuthRequestProvider()
         assert provider.get_name() == "OCI_REQUEST_AUTH"
 
-    @patch("oci_mlflow.auth_plugin.get_resource_principals_signer")
-    @patch.dict(os.environ, {"OCI_IAM_TYPE": "resource_principal"})
-    def test_get_auth_resource_principal(self, mock_get_signer):
-        mock_get_signer.return_value = MagicMock()
-        provider = OCIMLFlowAuthRequestProvider()
-        auth = provider.get_auth()
-        mock_get_signer.assert_called()
-        assert auth == mock_get_signer.return_value
-
-    @patch("oci_mlflow.auth_plugin.InstancePrincipalsSecurityTokenSigner")
-    @patch.dict(os.environ, {"OCI_IAM_TYPE": "instance_principal"})
-    def test_get_auth_instance_principal(self, mock_get_signer):
-        mock_get_signer.return_value = MagicMock()
-        provider = OCIMLFlowAuthRequestProvider()
-        auth = provider.get_auth()
-        assert auth == mock_get_signer.return_value
-        mock_get_signer.assert_called()
-
-    @patch("oci_mlflow.auth_plugin.SecurityTokenSigner")
-    @patch("oci.signer.load_private_key_from_file")
-    @patch("oci_mlflow.auth_plugin.utils._read_security_token_file")
-    @patch("oci_mlflow.auth_plugin.from_file")
-    @patch.dict(os.environ, {"OCI_IAM_TYPE": "security_token"})
-    def test_get_auth_security_token(
-        self,
-        mock_from_config,
-        mock_read_security_token_file,
-        mock_load_private_key,
-        mock_get_signer
-    ):
-        mock_from_config.return_value = {
-            "fingerprint": "123",
-            "key_file": "oci_api_key.pem",
-            "tenancy": "test_tenancy",
-            "region": "us-ashburn-1",
-            "security_token_file": "token"
+    @patch("oci_mlflow.auth_plugin.default_signer")
+    def test_get_auth(self, mock_default_signer):
+        mock_default_signer.return_value = {
+            "config": {},
+            "signer": "test_default_signer",
+            "client_kwargs": {},
         }
-        mock_read_security_token_file.return_value = "token"
-        mock_load_private_key.return_value = MagicMock()
-        mock_get_signer.return_value = MagicMock()
         provider = OCIMLFlowAuthRequestProvider()
         auth = provider.get_auth()
-        assert auth == mock_get_signer.return_value
-        mock_from_config.assert_called_with(
-            file_location="~/.oci/config",
-            profile_name="DEFAULT"
-        )
-        mock_read_security_token_file.assert_called_with(
-            "token"
-        )
-        mock_load_private_key.assert_called_with(
-            filename="oci_api_key.pem", 
-            pass_phrase=None
-        )
-        mock_get_signer.assert_called()
-
-    @patch("oci_mlflow.auth_plugin.Signer")
-    @patch("oci_mlflow.auth_plugin.from_file")
-    @patch.dict(os.environ, {"OCI_IAM_TYPE": "api_key"})
-    def test_get_auth_api_key(self, mock_from_config, mock_signer):
-        mock_from_config.return_value = {
-            "fingerprint": "123",
-            "key_file": "oci_api_key.pem",
-            "tenancy": "test_tenancy",
-            "region": "us-ashburn-1",
-            "user": "test_user"
-        }
-        mock_signer.return_value = MagicMock()
-        provider = OCIMLFlowAuthRequestProvider()
-        auth = provider.get_auth()
-        assert auth == mock_signer.return_value
-        mock_from_config.assert_called_with(
-            file_location="~/.oci/config",
-            profile_name="DEFAULT"
-        )
-        mock_signer.assert_called_with(
-            tenancy="test_tenancy",
-            user="test_user",
-            fingerprint="123",
-            private_key_file_location="oci_api_key.pem",
-            pass_phrase=None
-        )
-
-    @patch("oci_mlflow.auth_plugin.Signer")
-    @patch("oci_mlflow.auth_plugin.from_file")
-    @patch.dict(os.environ, {"OCI_IAM_TYPE": "invalid_type"})
-    def test_get_auth_invalid_type(self, mock_from_config, mock_signer):
-        mock_from_config.return_value = {
-            "fingerprint": "123",
-            "key_file": "oci_api_key.pem",
-            "tenancy": "test_tenancy",
-            "region": "us-ashburn-1",
-            "user": "test_user"
-        }
-        mock_signer.return_value = MagicMock()
-        provider = OCIMLFlowAuthRequestProvider()
-        auth = provider.get_auth()
-        assert auth == mock_signer.return_value
-        mock_from_config.assert_called_with(
-            file_location="~/.oci/config",
-            profile_name="DEFAULT"
-        )
-        mock_signer.assert_called_with(
-            tenancy="test_tenancy",
-            user="test_user",
-            fingerprint="123",
-            private_key_file_location="oci_api_key.pem",
-            pass_phrase=None
-        )
+        assert auth == "test_default_signer"
